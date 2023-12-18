e702c546e65fce3d8781a3ff8689e938bc2b3f68
Tatarcuka <kristiana.tatarcuka@accenture.com>
2023-12-18 09:47:18 +0200

    create project structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
-import './App.css';
+import "./App.css";
+import Home from "@Pages/Home";
+// import { BrowserRouter as Router } from 'react-router-dom';
+import { Route, Routes, Navigate, Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        
+      </Routes>
     </div>
   );
 }
