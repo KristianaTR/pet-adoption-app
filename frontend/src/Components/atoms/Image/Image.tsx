89f924da128354d69b0ef77ff9254b560b8646af
Tatarcuka <kristiana.tatarcuka@accenture.com>
2023-12-29 12:45:51 +0200

    create initial redux setup

--- frontend/src/Components/atoms/Image/Image.tsx
+++ frontend/src/Components/atoms/Image/Image.tsx
@@ -1,27 +1,20 @@
-import styled, { css } from "styled-components";
-import { ImageProps } from "./Image.types";
+import styled from 'styled-components';
+import { ImageProps } from './Image.types';
 
-interface StyledImageProps {
-  styledMode?: boolean;
-}
-
-const StyledImage = styled.img<StyledImageProps>`
-  ${(props) =>
-    props.styledMode
-      ? css`
-          max-width: 100%;
-          height: auto;
-          border-radius: 8px;
-          box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-        `
-      : css`
-          max-width: 100%;
-          height: auto;
-        `}
+const DefaultImage = styled.img`
+  max-width: 100%;
+  height: auto;
 `;
 
-const Image = ({ src, alt, mode }: ImageProps) => {
-  return <StyledImage src={src} alt={alt} styledMode={mode === "styled"}/>;
-};
+const StyledImage = styled(DefaultImage)`
+  border-radius: 8px;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+`;
 
-export default Image;
+const Image = ({ src, alt }: ImageProps) => {
+  return (
+    <StyledImage src={src} alt={alt} />
+  )
+}
+
+export default Image
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import store from './App/store';
 import { Provider } from 'react-redux';
+import store from "App/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
