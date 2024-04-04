11451070d9a609cf5ae8729bfdf744fc2924aa9c
KristianaTR <kristiana.tatarcuka@gmail.com>
2024-04-04 13:19:55 +0300

    add top boxShadow

--- frontend/src/Styles/Theme.ts
+++ frontend/src/Styles/Theme.ts
@@ -22,6 +22,7 @@ const theme: DefaultTheme = {
     default: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
     hover: "rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px",
     bottom: "rgba(0, 0, 0, 0.24) 0px 8px 8px -1px",
+    top: "rgba(0, 0, 0, 0.24) 0px -8px 8px -1px",
     minimal: "rgba(0, 0, 0, 0.18) 0px 2px 4px",
   },
   fontSize: {
