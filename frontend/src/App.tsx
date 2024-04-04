337564bf561db24340e2345f72299de3556d58df
KristianaTR <kristiana.tatarcuka@gmail.com>
2024-04-04 13:16:06 +0300

    add router link to ComingSoon page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import DogPage from "@Pages/DogPage";
 // import { BrowserRouter as Router } from 'react-router-dom';
 import { Route, Routes, Navigate, Outlet } from "react-router-dom";
 import PetDetailPage from "@Pages/PetDetailPage/PetDetailPage";
+import ComingSoonPage from "@Pages/ComingSoonPage";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/adopt" element={<AdoptPage />} />
         <Route path="/dog" element={<DogPage />} />
+        <Route path="/comingSoon" element={<ComingSoonPage />} />
         <Route path="/" element={<Outlet />}>
           {/* Add a nested route for individual dogs */}
           <Route path=":dogName" element={<PetDetailPage />} />
