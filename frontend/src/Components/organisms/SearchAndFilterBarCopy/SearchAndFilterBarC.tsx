f99e68af0a59f568c993f87d5506e27544047fac
KristianaTR <kristiana.tatarcuka@gmail.com>
2024-05-30 16:30:35 +0300

    create safe copy for testing

---
+++ frontend/src/Components/organisms/SearchAndFilterBarCopy/SearchAndFilterBarC.tsx
@@ -0,0 +1,59 @@
+import {
+  SearchAndFilterBarContainer,
+  SearchAndFilterBarHeading,
+} from "./SearchAndFilterBarC.style";
+import Button from "@Components/atoms/Button";
+import SearchInput from "../../molecules/SeachInput";
+import FilterPanel from "@Components/molecules/FilterPanel";
+import { useState } from "react";
+import { AnimatePresence } from "framer-motion";
+
+const SearchAndFilterBarC = () => {
+  const [isFilterPanelOpen, setIsFilterPanelOpen] = useState(false);
+
+  const filterOptions = [
+    {
+      category: "Age",
+      values: ["baby", "young", "adult", "senior", "any"],
+    },
+    {
+      category: "Gender",
+      values: ["male", "female", "any"],
+    },
+    {
+      category: "Size",
+      values: ["small", "medium", "large", "xlarge", "any"],
+    },
+  ];
+
+  const toggleFilterPanel = () => {
+    setIsFilterPanelOpen(!isFilterPanelOpen);
+    console.log("isFilterPanelOpen: " + isFilterPanelOpen);
+  };
+
+  // const handleSearch = () => {
+  //   console.log("Searching is active!");
+  // };
+
+  return (
+    <SearchAndFilterBarContainer>
+      <SearchAndFilterBarHeading>
+        <SearchInput />
+        <Button
+          text={!isFilterPanelOpen ? "Open filter" : "Close filter"}
+          onClick={toggleFilterPanel}
+        />
+      </SearchAndFilterBarHeading>
+      <AnimatePresence initial={false}>
+        {isFilterPanelOpen && (
+          <FilterPanel
+            filterOptions={filterOptions}
+            isOpen={isFilterPanelOpen}
+          />
+        )}
+      </AnimatePresence>
+    </SearchAndFilterBarContainer>
+  );
+};
+
+export default SearchAndFilterBarC;
